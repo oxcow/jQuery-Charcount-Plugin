ceec51e3da74f6e7d861996b17b28431cc5e3a90
leeyee <seadlead@gmail.com>
2013-01-04 11:53:09 +0800

    add conditions to mouseup.charcout event

--- jquery.charcount.js
+++ jquery.charcount.js
@@ -2,126 +2,128 @@
  * @author Gina
  */
 (function($) {
-    var CharCountCSS = {
-        divCss : {
-            margin : '0px 0px',
-            padding : '0px 0px',
-            position : 'absolute',
-            display : 'inline',
-            'z-index' : '10',
-            'font-family' : 'Constantia, Georgia',
-            'font-size' : '22px'
-        },
-        span2Css : {
-            color : 'teal',
-            'font-size' : '20px'
-        },
-        normalCss : {
-            'font-weight' : 'normal',
-            color : 'black'
-        },
-        overflowCss : {
-            'font-weight' : 'bold',
-            color : 'red'
-        }
-    };
+	var CharCountCSS = {
+		divCss : {
+			margin : '0px 0px',
+			padding : '0px 0px',
+			position : 'absolute',
+			display : 'inline',
+			'z-index' : '10',
+			'font-family' : 'Constantia, Georgia',
+			'font-size' : '22px'
+		},
+		span2Css : {
+			color : 'teal',
+			'font-size' : '20px'
+		},
+		normalCss : {
+			'font-weight' : 'normal',
+			color : 'black'
+		},
+		overflowCss : {
+			'font-weight' : 'bold',
+			color : 'red'
+		}
+	};
 
-    // 统计当前字符长度
-    function countChar($obj) {
-        return $obj.val() ? $obj.val().length : 0;
-    };
+	var isChromeOrMozilla = $.browser.hasOwnProperty("chrome") || $.browser.hasOwnProperty("mozilla");
 
-    function _defaultOverflowEvent(maxLen) {
-        $(this).val($(this).val().substring(0, maxLen));
-    };
+	// 统计当前字符长度
+	function countChar($obj) {
+		return $obj.val() ? $obj.val().length : 0;
+	};
 
-    // TODO: 可选择显示位置
-    // TODO: 自适应高度
-    $.fn.charcount = function(options) {
+	function _defaultOverflowEvent(maxLen) {
+		$(this).val($(this).val().substring(0, maxLen));
+	};
 
-        if ( typeof options === 'number') {
-            options = {
-                max : options
-            };
-        }
+	// TODO: 可选择显示位置
+	// TODO: 自适应高度
+	$.fn.charcount = function(options) {
 
-        var opts = $.extend(true, {}, $.fn.charcount.defaults, options);
+		if ( typeof options === 'number') {
+			options = {
+				max : options
+			};
+		}
 
-        var $div = $("<div>").css(CharCountCSS.divCss);
+		var opts = $.extend(true, {}, $.fn.charcount.defaults, options);
 
-        this.after($div);
+		var $div = $("<div>").css(CharCountCSS.divCss);
 
-        var $span1 = $("<span>");
+		this.after($div);
 
-        var $span2 = opts.max ? $("<span>").css(CharCountCSS.span2Css).text("/" + opts.max) : null;
+		var $span1 = $("<span>");
 
-        var offset = this.offset();
+		var $span2 = opts.max ? $("<span>").css(CharCountCSS.span2Css).text("/" + opts.max) : null;
 
-        $.data(this[0], "W", this.outerWidth());
-        $.data(this[0], "H", this.outerHeight());
+		var offset = this.offset();
 
-        this.bind("mouseup", function() {
-            var _dh = $(this).outerHeight();
-            var _dw = $(this).outerWidth();
+		var $self = this;
 
-            if ($.data(this, "W") != _dw || $.data(this, "H") != _dh) {
+		$div.bind("show.charcount", function(event, charLen) {
 
-                var div_offset = $div.offset();
+			$span1.html(charLen);
 
-                var _dtop = div_offset.top + (_dh - $.data(this, "H"));
-                var _dleft = div_offset.left + (_dw - $.data(this, "W"));
+			$(this).append($span1).append($span2);
 
-                $div.offset({
-                    top : _dtop,
-                    left : _dleft
-                });
+			var dtop = offset.top + $self.outerHeight() - $(this).height() - 2;
+			var dleft = offset.left + $self.outerWidth() - $(this).width() - 6;
 
-                $.data(this, "W", _dw);
-                $.data(this, "H", _dh);
+			$(this).offset({
+				top : dtop,
+				left : dleft
+			});
 
-            }
-        });
+		});
 
-        var $self = this;
+		this.bind("keyup.charcount", function() {
+			if (opts.max && countChar($(this)) > opts.max) {
+				$span1.css(CharCountCSS.overflowCss);
+				opts.overflowevt.call($self[0], opts.max);
+			} else {
+				$span1.css(CharCountCSS.normalCss);
+			}
+			$div.trigger("show.charcount", countChar($(this)));
+		});
 
-        $div.bind("show.charcount", function(event, charLen) {
+		// 鼠标粘贴、剪贴事件
+		this.bind("paste.charcount cut.charcount", function() {
+			setTimeout(function() {
+				$self.trigger('keyup.charcount');
+			}, 5);
+		});
 
-            $span1.html(charLen);
+		if (isChromeOrMozilla && this.is("textarea")) {
 
-            $(this).append($span1).append($span2);
+			$.data(this[0], "W", this.outerWidth());
+			$.data(this[0], "H", this.outerHeight());
 
-            var dtop = offset.top + $self.outerHeight() - $(this).height() - 2;
-            var dleft = offset.left + $self.outerWidth() - $(this).width() - 6;
+			this.bind("mouseup.charcount", function() {
+				var _dh = $(this).outerHeight();
+				var _dw = $(this).outerWidth();
 
-            //console.info(dh, dw, dtop, dleft);
+				if ($.data(this, "W") != _dw || $.data(this, "H") != _dh) {
 
-            $(this).offset({
-                top : dtop,
-                left : dleft
-            });
+					var div_offset = $div.offset();
 
-        });
+					var _dtop = div_offset.top + (_dh - $.data(this, "H"));
+					var _dleft = div_offset.left + (_dw - $.data(this, "W"));
 
-        this.bind("keyup.charcount", function() {
-            if (opts.max && countChar($(this)) > opts.max) {
-                $span1.css(CharCountCSS.overflowCss);
-                opts.overflowevt.call($self[0], opts.max);
-            } else {
-                $span1.css(CharCountCSS.normalCss);
-            }
-            $div.trigger("show.charcount", countChar($(this)));
-        });
+					$div.offset({
+						top : _dtop,
+						left : _dleft
+					});
 
-        // 鼠标粘贴、剪贴事件
-        this.bind("paste.charcount cut.charcount", function() {
-            setTimeout(function() {
-                $self.trigger('keyup.charcount');
-            }, 5);
-        });
-    };
+					$.data(this, "W", _dw);
+					$.data(this, "H", _dh);
+				}
+			});
+		}
+	};
 
-    $.fn.charcount.defaults = {
-        max : 0,
-        overflowevt : _defaultOverflowEvent
-    };
+	$.fn.charcount.defaults = {
+		max : 0,
+		overflowevt : _defaultOverflowEvent
+	};
 })(jQuery);
